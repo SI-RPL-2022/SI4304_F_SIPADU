0c7f73539e9506a27a0d62ede06bd7a0bcc84450
madegangga <madeganggaw@student.telkomuniversity.ac.id>
2022-04-09 14:31:18 +0700

    menambahkan controller laporan ref #6

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -1,17 +1,17 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
+ 
 use App\Models\Laporan;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-
+ 
 class LaporanController extends Controller
 {
-
+ 
     /** 
      * Display a listing of the resource.
      *
@@ -24,4 +24,28 @@ class LaporanController extends Controller
         ];
         return view('laporan.lapor_keluhan', $data);
     }
+ 
+    public function laporKeluhan(Request $request, $type, $id = null)
+    {
+ 
+        if ($type == 'fasilitas-rusak') {
+            $data = [
+                'title' => 'Lapor Fasilitas Rusak'
+            ];
+ 
+            if ($id != null) {
+                $laporan = Laporan::find($id);
+                if ($laporan == null) abort(404);
+ 
+                $data['laporan'] = $laporan;
+ 
+                return view('laporan.lapor_fasilitas_rusak_upload', $data);
+            }
+ 
+            return view('laporan.lapor_fasilitas_rusak', $data);
+        } else {
+            return abort(404);
+        }
+    }
 }
+ 

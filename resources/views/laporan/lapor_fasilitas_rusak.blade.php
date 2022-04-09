5fe4a8a8bb43b6921e049601bab35654a009f9af
Vylladp <anantarevyolla@student.telkomuniversity.ac.id>
2022-04-09 14:15:15 +0700

    Melanjutkan view laporan fasilitas rusak ref #6

--- resources/views/laporan/lapor_fasilitas_rusak.blade.php
+++ resources/views/laporan/lapor_fasilitas_rusak.blade.php
@@ -14,7 +14,32 @@
                         <div class="border-bottom mx-4 pb-3">
                             <img src="{{ asset('images/step-1.svg') }}" width="100%" alt="">
                         </div>
+                        <div class="py-3 text-start px-4">
+                            <div class="form-group mb-4">
+                                <label class="mb-2 ms-3"><b>Pilih Fasilitas</b></label>
+                                <select name="fasilitas" id="fasilitas" class="form-control py-2 px-3 rounded-pill border-0"
+                                    style="box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.1);" required>
+                                    <option value="">Pilih Fasilitas</option>
+                                    <option value="Lainnya">Lainnya</option>
+                                </select>
+                            </div>
+                            <div class="form-group mb-4">
+                                <label class="mb-2 ms-3"><b>Lokasi</b></label>
+                                <input type="text" name="lokasi" class="form-control py-2 px-3 rounded-pill border-0"
+                                    style="box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.1);" placeholder="Tuliskan Lokasinya"
+                                    required>
+                            </div>
+                            <div class="form-group mb-4">
+                                <label class="mb-2 ms-3"><b>Detail Keluhan</b></label>
+                                <input type="text" name="keluhan" class="form-control py-2 px-3 rounded-pill border-0"
+                                    style="box-shadow: 0px 4px 14px rgba(0, 0, 0, 0.1);" placeholder="Detail Keluhan"
+                                    required>
+                            </div>
+                        </div>
                     </div>
+                </div>
+                <div class="d-flex justify-content-end mt-3">
+                    <button class="btn btn-primary-2">Selanjutnya</button>
                 </div>
             </div>
         </form>

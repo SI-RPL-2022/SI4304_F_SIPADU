cf4bd4fb2034c640b2376b515ed4a8aaf7e37127
madegangga <madeganggaw@student.telkomuniversity.ac.id>
2022-04-09 14:44:06 +0700

    membuat model laporan ref #6

---
+++ app/Models/Laporan.php
@@ -0,0 +1,33 @@
+<?php
+ 
+namespace App\Models;
+ 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+ 
+class Laporan extends Model
+{
+    use HasFactory;
+ 
+    protected $table = 'laporan';
+    public $incrementing = true;
+    public $timestamps = true;
+    protected $fillable = [
+        'id_user',
+        'no_referensi',
+        'fasilitas',
+        'lokasi',
+        'keluhan',
+        'file',
+        'tipe',
+        'status',
+        'alasan_penolakan',
+    ];
+ 
+    const STATUS = [
+        '0' => 'Menunggu Tanggapan',
+        '1' => 'Diterima',
+        '99' => 'Ditolak'
+    ];
+}
+ 

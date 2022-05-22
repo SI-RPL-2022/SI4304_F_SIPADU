da794afdb38491687feb1e63019c9cb953778f5c
itsnaufal <noriko.kazuko@gmail.com>
2022-05-22 08:22:52 +0700

    Menambahkan Model Laporan ref #11

--- app/Models/Laporan.php
+++ app/Models/Laporan.php
@@ -1,14 +1,14 @@
 <?php
- 
+
 namespace App\Models;
- 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
- 
+
 class Laporan extends Model
 {
     use HasFactory;
- 
+
     protected $table = 'laporan';
     public $incrementing = true;
     public $timestamps = true;
@@ -23,11 +23,10 @@ class Laporan extends Model
         'status',
         'alasan_penolakan',
     ];
- 
+
     const STATUS = [
         '0' => 'Menunggu Tanggapan',
         '1' => 'Diterima',
         '99' => 'Ditolak'
     ];
 }
- 

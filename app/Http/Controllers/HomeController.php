58ac09cb2641a1fccc26d1ebc709fe66afe172d9
itsnaufal <noriko.kazuko@gmail.com>
2022-05-22 08:20:19 +0700

    Updating homecontroller.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,7 +44,35 @@ class HomeController extends Controller
             'user' => $user
         ];
 
-        return view('profil.index', $data);
+        if ($request->segment(2) == 'edit') {
+            if ($request->segment(3) == 'submit') {
+                $userEmail = User::where('email', $request->email)->where('id', '!=', Auth::user()->id)->first();
+                if($userEmail){
+                    $request->session()->flash('alert', 'danger');
+                    $request->session()->flash('message', 'Email telah digunakan!');
+                    return redirect()->back();
+                }
+                $user->email = $request->email;
+                $user->address = $request->address;
+                $user->phone = $request->phone;
+                if($request->password != null){
+                    if($request->password == $request->password_confirmation){
+                        $user->password = Hash::make($request->password);
+                    }else {
+                        $request->session()->flash('alert', 'danger');
+                        $request->session()->flash('message', 'Password konfirmasi tidak sesuai!');
+                        return redirect()->back();
+                    }
+                }
+                $user->save();
 
+                $request->session()->flash('alert', 'success');
+                $request->session()->flash('message', 'User Berhasil Diubah!');
+                return redirect()->to(route('profil'));
+            }
+            return view('profil.edit', $data);
+        }
+
+        return view('profil.index', $data);
     }
 }

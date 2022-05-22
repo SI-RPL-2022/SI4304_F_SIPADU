443f95f6679e238c9b273fffd7e9cb9a6bf283df
itsnaufal <noriko.kazuko@gmail.com>
2022-05-22 08:21:28 +0700

    Updating LaporanController.php

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -1,17 +1,17 @@
 <?php
- 
+
 namespace App\Http\Controllers;
- 
+
 use App\Models\Laporan;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
- 
+
 class LaporanController extends Controller
 {
- 
+
     /** 
      * Display a listing of the resource.
      *
@@ -24,30 +24,30 @@ class LaporanController extends Controller
         ];
         return view('laporan.lapor_keluhan', $data);
     }
- 
+
     public function laporKeluhan(Request $request, $type, $id = null)
     {
- 
+
         if ($type == 'fasilitas-rusak') {
             $data = [
                 'title' => 'Lapor Fasilitas Rusak'
             ];
- 
+
             if ($id != null) {
                 $laporan = Laporan::find($id);
                 if ($laporan == null) abort(404);
- 
+
                 $data['laporan'] = $laporan;
- 
+
                 return view('laporan.lapor_fasilitas_rusak_upload', $data);
             }
- 
+
             return view('laporan.lapor_fasilitas_rusak', $data);
         } else {
             return abort(404);
         }
     }
- 
+
     /**
      * Store a newly created resource in storage.
      *
@@ -67,9 +67,9 @@ class LaporanController extends Controller
                 'tipe' => $request->tipe,
                 'status' => 0,
             ]);
- 
+
             DB::commit();
- 
+
             $request->session()->flash('alert', 'success');
             $request->session()->flash('message', 'Laporan Berhasil Ditambahkan!');
             if ($request->tipe == 1) {
@@ -80,7 +80,7 @@ class LaporanController extends Controller
             throw $e;
         }
     }
- 
+
     public function uploadFasilitasRusak(Request $request, $id)
     {
         DB::beginTransaction();
@@ -90,7 +90,7 @@ class LaporanController extends Controller
                 $name = Str::random(7);
                 $newName = 'fasilitas_rusak_' . $name . $format;
                 $request->file('file')->move(public_path() . '/keluhan', $newName);
- 
+
                 $laporan = Laporan::find($id);
                 $laporan->file = $newName;
                 $laporan->save();
@@ -101,22 +101,108 @@ class LaporanController extends Controller
             }
             $request->session()->flash('noRef', $laporan->no_referensi);
             DB::commit();
- 
+
             return redirect()->to(route('lapor.keluhan.fasilitas.rusak.done'));
         } catch (\Exception $e) {
             throw $e;
         }
     }
- 
+
     public function daftarKeluhan()
     {
         $laporan = Laporan::orderBy('id', 'desc');
- 
+
         if (Auth::user()->role == 'user') {
             $laporan->where('id_user', Auth::user()->id);
         }
- 
-        return view('laporan.daftar_laporan');
+
+        $data = [
+            'title' => 'Daftar Laporan',
+            'laporan' => $laporan->get()
+        ];
+
+        return view('laporan.daftar_laporan', $data);
     }
-    
+
+    public function verifikasi(Request $request, $id)
+    {
+        $lapor = Laporan::find($id);
+        if ($lapor == null) abort(404);
+
+        $lapor->status = 1;
+
+        $data = [
+            'title' => 'Verifikasi',
+            'id' => $id,
+            'message' => "Laporan <b>#$lapor->no_referensi</b> telah berhasil diverifikasi"
+        ];
+
+        if (isset($request->lapor) && $request->lapor == 'tolak') {
+            if (isset($request->alasan)) {
+                $lapor->alasan_penolakan = $request->alasan;
+                $lapor->status = 99;
+
+                $data['message'] = "Laporan <b>#$lapor->no_referensi</b> telah berhasil ditolak";
+            } else {
+                return view('laporan.form_penolakan', $data);
+            }
+        }
+        $lapor->save();
+
+        return view('laporan.lapor_berhasil', $data);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $laporan = Laporan::find($id);
+
+        if ($laporan == null) abort(404);
+
+        $data = [
+            'title' => 'Detail Laporan',
+            'laporan' => $laporan
+        ];
+
+        return view('laporan.detail_laporan', $data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }

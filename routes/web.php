549e0c84152a1dc3a798d81c11d4a020b3a04a6b
DanarPrayoga <danarprayoga1@gmail.com>
2022-05-29 22:38:39 +0700

    menambah route untuk fasilitas #13

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FasilitasController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LaporanController;
 use App\Models\Laporan;
@@ -24,13 +25,17 @@ Route::get('/', function () {
 Route::get('/register/done', function () {
     return view('auth.register_done');
 });
-
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('profil', [HomeController::class, 'profil'])->name('profil');
 Route::get('profil/edit', [HomeController::class, 'profil'])->name('profil.edit');
 Route::post('profil/edit/submit', [HomeController::class, 'profil'])->name('profil.submit');
+
+Route::prefix('fasilitas')->group(function () {
+    Route::get('', [FasilitasController::class, 'index'])->name('fasilitas.index');
+    Route::get('{id}', [FasilitasController::class, 'show'])->name('fasilitas.show');
+});
 
 // Lapor Keluhan
 Route::prefix('lapor')->group(function () {
@@ -44,6 +49,26 @@ Route::prefix('lapor')->group(function () {
         $data['title'] = 'Laporan Fasilitas Rusak';
         return view('laporan.lapor_fasilitas_rusak_done', $data);
     })->name('lapor.keluhan.fasilitas.rusak.done');
+
+    Route::post('keluhan/oknum/{id}/upload', [LaporanController::class, 'uploadOknum'])->name('lapor.keluhan.upload.oknum');
+    Route::get('keluhan/oknum/done', function () {
+        $data['title'] = 'Lapor Oknum Perusak Fasilitas';
+        return view('laporan.oknum.lapor_oknum_done', $data);
+    })->name('lapor.keluhan.oknum.done');
+
+    Route::post('keluhan/saran/{id}/upload', [LaporanController::class, 'uploadSaran'])->name('lapor.keluhan.upload.saran');
+    Route::get('keluhan/saran/done', function () {
+        $data['title'] = 'Keluhan/Saran Terkait Fasilitas';
+        return view('laporan.saran.lapor_saran_done', $data);
+    })->name('lapor.keluhan.saran.done');
+
+    Route::post('feedback', [LaporanController::class, 'feedback'])->name('feedback.store');
+    Route::get('keluhan/saran/done', function () {
+        $data['title'] = 'Feedback Berhasil';
+        return view('laporan.feedback_done', $data);
+    })->name('lapor.feedback.done');
+
+
     Route::post('keluhan/save}', [LaporanController::class, 'store'])->name('lapor.keluhan.save');
 
     // Verifikasi

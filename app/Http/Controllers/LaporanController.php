12d716c3035b2d4a0e04390c5d90e572d795c867
sekartamaraa <sekartamaraa@student.telkomuniversity.ac.id>
2022-05-29 16:10:53 +0700

    necessary file

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AssignPetugas;
+use App\Models\Feedback;
 use App\Models\Laporan;
+use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -43,6 +46,36 @@ class LaporanController extends Controller
             }
 
             return view('laporan.lapor_fasilitas_rusak', $data);
+        } else if ($type == 'oknum') {
+            $data = [
+                'title' => 'Lapor Oknum Perusak Fasilitas'
+            ];
+
+            if ($id != null) {
+                $laporan = Laporan::find($id);
+                if ($laporan == null) abort(404);
+
+                $data['laporan'] = $laporan;
+
+                return view('laporan.oknum.lapor_oknum_upload', $data);
+            }
+
+            return view('laporan.oknum.lapor_oknum', $data);
+        } elseif ($type == 'saran') {
+            $data = [
+                'title' => 'Keluhan/Saran Terkait Fasilitas'
+            ];
+
+            if ($id != null) {
+                $laporan = Laporan::find($id);
+                if ($laporan == null) abort(404);
+
+                $data['laporan'] = $laporan;
+
+                return view('laporan.saran.lapor_saran_upload', $data);
+            }
+
+            return view('laporan.saran.lapor_saran', $data);
         } else {
             return abort(404);
         }
@@ -65,6 +98,9 @@ class LaporanController extends Controller
                 'lokasi' => $request->lokasi,
                 'keluhan' => $request->keluhan,
                 'tipe' => $request->tipe,
+                'tanggal_kejadian' => isset($request->tanggal_kejadian) ?  $request->tanggal_kejadian : null,
+                'waktu_kejadian' => isset($request->waktu_kejadian) ?  $request->waktu_kejadian : null,
+                'oknum' => isset($request->oknum) ?  $request->oknum : null,
                 'status' => 0,
             ]);
 
@@ -74,8 +110,28 @@ class LaporanController extends Controller
             $request->session()->flash('message', 'Laporan Berhasil Ditambahkan!');
             if ($request->tipe == 1) {
                 return redirect()->to(route('lapor.keluhan.upload.fasilitas.rusak', ['id' => $laporan->id]));
+            } else if ($request->tipe == 2) {
+                return redirect()->to(route('lapor.keluhan.upload.oknum', ['id' => $laporan->id]));
+            } else if ($request->tipe == 3) {
+                return redirect()->to(route('lapor.keluhan.upload.saran', ['id' => $laporan->id]));
             }
             return redirect()->to(route('lapor.keluhan'));
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+    public function feedback(Request $request)
+    {
+        DB::beginTransaction();
+        try {
+            $feedback = new Feedback();
+            $feedback->id_laporan = $request->id_laporan;
+            $feedback->feedback = $request->feedback;
+            $feedback->pesan = $request->pesan;
+            $feedback->save();
+
+            DB::commit();
+            return redirect()->to(route('lapor.feedback.done'));
         } catch (\Exception $e) {
             throw $e;
         }
@@ -103,6 +159,60 @@ class LaporanController extends Controller
             DB::commit();
 
             return redirect()->to(route('lapor.keluhan.fasilitas.rusak.done'));
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function uploadOknum(Request $request, $id)
+    {
+        DB::beginTransaction();
+        try {
+            if ($request->hasFile('file')) {
+                $format = $request->file('file')->getClientOriginalName();
+                $name = Str::random(7);
+                $newName = 'oknum_' . $name . $format;
+                $request->file('file')->move(public_path() . '/keluhan', $newName);
+
+                $laporan = Laporan::find($id);
+                $laporan->file = $newName;
+                $laporan->save();
+            } else {
+                $request->session()->flash('alert', 'warning');
+                $request->session()->flash('message', 'Harap Upload File Terlebih Dahulu!');
+                return redirect()->back();
+            }
+            $request->session()->flash('noRef', $laporan->no_referensi);
+            DB::commit();
+
+            return redirect()->to(route('lapor.keluhan.oknum.done'));
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function uploadSaran(Request $request, $id)
+    {
+        DB::beginTransaction();
+        try {
+            if ($request->hasFile('file')) {
+                $format = $request->file('file')->getClientOriginalName();
+                $name = Str::random(7);
+                $newName = 'saran_' . $name . $format;
+                $request->file('file')->move(public_path() . '/keluhan', $newName);
+
+                $laporan = Laporan::find($id);
+                $laporan->file = $newName;
+                $laporan->save();
+            } else {
+                $request->session()->flash('alert', 'warning');
+                $request->session()->flash('message', 'Harap Upload File Terlebih Dahulu!');
+                return redirect()->back();
+            }
+            $request->session()->flash('noRef', $laporan->no_referensi);
+            DB::commit();
+
+            return redirect()->to(route('lapor.keluhan.saran.done'));
         } catch (\Exception $e) {
             throw $e;
         }
@@ -146,8 +256,17 @@ class LaporanController extends Controller
             } else {
                 return view('laporan.form_penolakan', $data);
             }
+        } else if (isset($request->lapor) && $request->lapor == 'assign_petugas') {
+            $data['petugas'] = User::where('role', 'petugas')->get();
+            return view('laporan.assign_petugas', $data);
         }
         $lapor->save();
+
+        $assign = new AssignPetugas();
+        $assign->id_user = $request->petugas;
+        $assign->id_laporan = $id;
+        $assign->description = $request->description;
+        $assign->save();
 
         return view('laporan.lapor_berhasil', $data);
     }

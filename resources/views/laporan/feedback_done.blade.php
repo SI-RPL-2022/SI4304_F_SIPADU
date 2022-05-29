fb2b77b935bf93a57d8a44d2439ada81aa875dd4
Vylladp <anantarevyolla@student.telkomuniversity.ac.id>
2022-05-29 22:32:05 +0700

    membuat feedback #15

---
+++ resources/views/laporan/feedback_done.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="text-center">
+            <h2 class="text-primary-2 mb-4"><b>BERHASIL</b></h2>
+        </div>
+        <div class="card-register mx-auto">
+
+

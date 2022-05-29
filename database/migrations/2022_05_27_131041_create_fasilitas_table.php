8212da2317df0fab7cdc8b9d084ce4ba26140943
DanarPrayoga <danarprayoga1@gmail.com>
2022-05-29 23:51:26 +0700

    Menambahkan create fasilitas table ref #13

---
+++ database/migrations/2022_05_27_131041_create_fasilitas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFasilitasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fasilitas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('image')->nullable();
+            $table->string('maps')->nullable();
+            $table->string('deskripsi')->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('buka')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fasilitas');
+    }
+}

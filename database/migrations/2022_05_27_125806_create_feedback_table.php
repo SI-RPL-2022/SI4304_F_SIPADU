43f94c68bcbb502084406fd6483abfa62733ae02
DanarPrayoga <danarprayoga1@gmail.com>
2022-05-29 23:32:29 +0700

    menambahkan create feedback table ref #15

---
+++ database/migrations/2022_05_27_125806_create_feedback_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_laporan')->nullable();
+            $table->string('feedback')->nullable();
+            $table->string('pesan')->nullable();
+            $table->timestamps();
+
+            $table->foreign('id_laporan')->references('id')->on('laporan')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}

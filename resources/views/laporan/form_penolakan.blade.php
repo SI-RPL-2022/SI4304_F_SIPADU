d8a0d0eaf317a8dd99e741303b49fc0711f4fd24
Vylladp <anantarevyolla@student.telkomuniversity.ac.id>
2022-05-30 00:00:40 +0700

    Membuat laporan penolakan #7

---
+++ resources/views/laporan/form_penolakan.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="d-flex justify-content-between">
+            <h2 class="text-primary-2">
+                <b>ALASAN PENOLAKAN LAPORAN</b>
+            </h2>
+            <a href="#" class="text-primary-2"
+                style="font-size: 1.3em; text-decoration: none; vertical-align: bottom;">Kembali</a>
+        </div>
+

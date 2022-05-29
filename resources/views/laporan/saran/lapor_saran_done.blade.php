fca4cfb996d26e28d409efc3a79cc0a1ba5e1d47
madegangga <madeganggaw@student.telkomuniversity.ac.id>
2022-05-30 00:26:56 +0700

    membuat feedback berhasil upload lapor saran #9

---
+++ resources/views/laporan/saran/lapor_saran_done.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="text-center">
+            <h2 class="text-primary-2 mb-4"><b>Keluhan/Saran Terkait Fasilitas</b></h2>
+        </div>
+        <div class="card-register mx-auto">
+
+            <div class="card shadow-sm border-0 pt-3 pb-3" style="border-radius: 35px;">
+                <div class="card-body text-center">
+                    <div class="border-bottom mx-4 pb-3">
+                        <img src="{{ asset('images/step-3.svg') }}" width="100%" alt="">
+                    </div>
+                    <div class="py-3 text-center px-4">
+                        <img src="{{ asset('images/success.svg') }}" class="my-3" alt="">
+                        <p class="text-primary-2 mt-4 px-md-5">
+                            Laporan Berhasil Di Kirimkan
+                            Nomor Laporan Anda Adalah <b>#{{ Session::get('noRef') }}</b>
+                            Silahkan tunggu Update Laporannya di Menu Status Laporan Secara Berkala
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

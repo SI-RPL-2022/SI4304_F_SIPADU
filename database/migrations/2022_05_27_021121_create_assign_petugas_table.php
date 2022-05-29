52dcb05ed7279ec9d366e0f2a852013d6a22d949
DanarPrayoga <danarprayoga1@gmail.com>
2022-05-29 23:15:19 +0700

    add database create assign petugas #12

---
+++ database/migrations/2022_05_27_021121_create_assign_petugas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssignPetugasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assign_petugas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_laporan');
+            $table->text('description');
+            $table->timestamps();
+
+
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('id_laporan')->references('id')->on('laporan')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assign_petugas');
+    }
+}

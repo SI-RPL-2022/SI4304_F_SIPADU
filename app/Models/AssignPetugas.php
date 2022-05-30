b21c111e854e4a5d73529c3a0ed11368a61e7ee2
sekartamaraa <sekartamaraa@student.telkomuniversity.ac.id>
2022-05-30 10:38:30 +0700

    Mengupdate assign petugas #12

--- app/Models/AssignPetugas.php
+++ app/Models/AssignPetugas.php
@@ -11,4 +11,9 @@ class AssignPetugas extends Model
     protected $table = 'assign_petugas';
     public $incrementing = true;
     public $timestamps = true;
+
+    public function User()
+    {
+        return $this->hasOne('App\Models\User', 'id', 'id_user');
+    }
 }
